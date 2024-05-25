ea339ad7d39f08737a06d2147b8ca249176b0b57
Mehdi Ben Hadj Khelifa <mehdi.bhk13@gmail.com>
2024-05-26 00:20:24 +0100

    Added piping support for commands

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use std::{
-    env, io::{stdin, stdout, Write}, path::Path, process::Command
+    env,
+    io::{stdin, stdout, Write},
+    path::Path,
+    process::{Child, Command, Stdio},
 };
 
 fn main() {
@@ -7,30 +10,57 @@ fn main() {
         print!("> ");
         stdout().flush().expect("Error flushing stdout");
         let mut input: String = String::new();
-        let command: &str;
         stdin().read_line(&mut input).expect("Error reading input");
-        let mut parts = input.trim().split_whitespace();
-        command = parts.next().expect("Invalid Command");
-        let args = parts;
-        match command {
-            "cd" => {
-                // default to '/' as new directory if one was not provided
-                let new_dir = args.peekable().peek().map_or("/",|x| *x);
-                let root = Path::new(new_dir) ;
-                if let Err(e) = env::set_current_dir(&root){
-                    eprintln!("{}",e);
+        let mut commands = input.trim().split(" | ").peekable();
+        let mut previous_command = None;
+        while let Some(command) = commands.next() {
+            let mut parts = command.trim().split_whitespace();
+            let command = parts.next().expect("Invalid Command");
+            let args = parts;
+            match command {
+                "cd" => {
+                    // default to '/' as new directory if one was not provided
+                    let new_dir = args.peekable().peek().map_or("/", |x| *x);
+                    let root = Path::new(new_dir);
+                    if let Err(e) = env::set_current_dir(&root) {
+                        eprintln!("{}", e);
+                    }
+                    previous_command = None;
                 }
-            },
-            "exit" => return,
-            command =>{
-                let child = Command::new(command)
-                    .args(args)
-                    .spawn();
-                match child{
-                    Ok(mut child) => {let _ = child.wait();},
-                    Err(e) => eprintln!("{}",e),
-                };
+                "exit" => return,
+                command => {
+                    let stdin = previous_command.map_or(Stdio::inherit(), |output: Child| {
+                        Stdio::from(output.stdout.unwrap())
+                    });
+                    let stdout = if commands.peek().is_some() {
+                        // there is another command piped behind this one
+                        // prepare to send output to the next command
+                        Stdio::piped()
+                    } else {
+                        // there are no more commands piped behind this one
+                        // send output to shell stdout
+                        Stdio::inherit()
+                    };
+                    let output = Command::new(command)
+                        .args(args)
+                        .stdin(stdin)
+                        .stdout(stdout)
+                        .spawn();
+                    match output {
+                        Ok(output) => {
+                            previous_command = Some(output);
+                        }
+                        Err(e) => {
+                            previous_command = None;
+                            eprintln!("{}", e);
+                        }
+                    }
+                }
             }
+        }
+        if let Some(mut final_command) = previous_command {
+            // block until the final command has finished
+            let _ = final_command.wait();
         }
     }
 }
