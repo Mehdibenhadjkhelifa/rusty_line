75e75e0f056563f69a2d66c06ad38618fd477922
Mehdi Ben Hadj Khelifa <mehdi.bhk13@gmail.com>
2024-05-25 23:55:41 +0100

    Added cd support for the shell

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use std::{
-    io::{stdin, stdout, Write},
-    process::Command,
+    env, io::{stdin, stdout, Write}, path::Path, process::Command
 };
 
 fn main() {
@@ -13,10 +12,22 @@ fn main() {
         let mut parts = input.trim().split_whitespace();
         command = parts.next().expect("Invalid Command");
         let args = parts;
-        let mut child = Command::new(command)
-            .args(args)
-            .spawn()
-            .expect("Error Executing command ");
-        child.wait().unwrap();
+        match command {
+            "cd" => {
+                // default to '/' as new directory if one was not provided
+                let new_dir = args.peekable().peek().map_or("/",|x| *x);
+                let root = Path::new(new_dir) ;
+                if let Err(e) = env::set_current_dir(&root){
+                    eprintln!("{}",e);
+                }
+            },
+            command =>{
+                let mut child = Command::new(command)
+                    .args(args)
+                    .spawn()
+                    .expect("Error Executing command ");
+                child.wait().unwrap();
+            }
+        }
     }
 }
